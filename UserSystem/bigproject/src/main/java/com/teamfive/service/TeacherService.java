d84ca1b590c80bda530bf59f0d5861c9a58583b8
Zi_Lok <46697864+WongZiLok@users.noreply.github.com>
2019-01-28 12:00:41 +0800

    Add files via upload

--- UserSystem/bigproject/src/main/java/com/teamfive/service/TeacherService.java
+++ UserSystem/bigproject/src/main/java/com/teamfive/service/TeacherService.java
@@ -1,12 +1,19 @@
 package com.teamfive.service;
 
+import com.teamfive.pojo.PageInfo;
 import com.teamfive.pojo.Student;
 import com.teamfive.pojo.Teacher;
 
 public interface TeacherService {
 	Teacher checkLogin(String t_username, String t_password);
+	
+	PageInfo showPage(String s_name,String t_name,String pageSize,String pageNumber);
+
 	void RegistS(Student student);
+
 	void RegistT(Teacher teacher);
+
 	void updateTeac(Teacher teacher);
+
 	void updateTPW(Teacher teacher);
 }
--- UserSystem/bigproject/src/main/java/com/teamfive/service/TeacherServiceImpl.java
+++ UserSystem/bigproject/src/main/java/com/teamfive/service/TeacherServiceImpl.java
@@ -1,19 +1,25 @@
 package com.teamfive.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.teamfive.dao.StudentMapper;
 import com.teamfive.dao.TeacherMapper;
+import com.teamfive.pojo.PageInfo;
 import com.teamfive.pojo.Student;
 import com.teamfive.pojo.Teacher;
 
 @Service
-
 public class TeacherServiceImpl implements TeacherService {
 	@Autowired
 	TeacherMapper teacherMapper;
 
+	@Autowired
+	StudentMapper studentMapper;
+	
 	@Override
 	public Teacher checkLogin(String t_username, String t_password) {
 		Teacher teacher = teacherMapper.findBYUsername(t_username);
@@ -52,7 +58,7 @@ public class TeacherServiceImpl implements TeacherService {
 	@Override
 	public void updateTeac(Teacher teacher) {
 		try {
-			teacherMapper.updateTeac(teacher.getSex(), teacher.getBirthday(), teacher.getT_phone(), teacher.getT_password(), teacher.getTid());
+			teacherMapper.updateTeac(teacher.getSex(), teacher.getBirthday(), teacher.getT_phone(), teacher.getTid());
 		} catch (Exception e) {
 			throw new RuntimeException("出现异常，事务回滚");
 		}
@@ -66,5 +72,37 @@ public class TeacherServiceImpl implements TeacherService {
 			throw new RuntimeException("出现异常，事务回滚");
 		}
 	}
+
+	@Override
+	public PageInfo showPage(String s_name, String t_name, String pageSizeStr, String pageNumberStr) {
+		
+		Integer pageSize = 4;
+		if(pageSizeStr!=null&&!pageSizeStr.equals("")){
+			pageSize = Integer.parseInt(pageSizeStr);
+		}
+		Integer pageNumber = 1;
+		if(pageNumberStr!=null&&!pageNumberStr.equals("")){
+			pageNumber = Integer.parseInt(pageNumberStr);
+		}
+		
+		PageInfo pInfo = new PageInfo();
+		pInfo.setPageNumber(pageNumber);
+		pInfo.setPageSize(pageSize);
+		pInfo.setPageStart((pageNumber-1)*pageSize);
+		pInfo.setS_name(s_name);
+		pInfo.setT_name(t_name);
+		
+		List<Student> list = (List<Student>) teacherMapper.selByPage(pInfo);
+		
+		for (Student student : list) {
+			student.setTeacher(teacherMapper.findById(student.getTid()));
+		}
+		pInfo.setList(list);
+		Long count = teacherMapper.selCountByPageInfo(pInfo);
+		
+		pInfo.setTotal(count%pageSize==0?count/pageSize:count/pageSize+1);
+		return pInfo;
+	}
+
 
 }
