263a3d4ed1bffc3ddba1321a0205788ec8271c23
Zi_Lok <46697864+WongZiLok@users.noreply.github.com>
2019-01-28 11:55:31 +0800

    Add files via upload

--- UserSystem/bigproject/src/main/java/com/teamfive/controller/TeacherController.java
+++ UserSystem/bigproject/src/main/java/com/teamfive/controller/TeacherController.java
@@ -1,5 +1,6 @@
 package com.teamfive.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,25 +11,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
-
+import com.teamfive.pojo.PageInfo;
 import com.teamfive.pojo.Student;
 import com.teamfive.pojo.Teacher;
 import com.teamfive.service.TeacherService;
 
 @Controller
 @RequestMapping("/teacher")
-@SessionAttributes("teacher")
+@SessionAttributes(value= {"teacher","pInfo"})
 public class TeacherController {
 	@Autowired
 	TeacherService teacherService;
 
-	//跳转登录
+	// 跳转登录
 	@RequestMapping("/login")
 	public String login() {
 		return "login";
 	}
 
-	//登录验证
+	// 登录验证
 	@RequestMapping("/checkLogin")
 	public String checkLoing(Teacher teacher, Model model) {
 		teacher = teacherService.checkLogin(teacher.getT_username(), teacher.getT_password());
@@ -45,7 +46,7 @@ public class TeacherController {
 		return "login";
 	}
 
-	//登出
+	// 登出
 	@RequestMapping("/outLogin")
 	public String outLogin(HttpSession session, SessionStatus sessionStatus) {
 		session.removeAttribute("teacher");
@@ -54,15 +55,15 @@ public class TeacherController {
 		return "login";
 	}
 
-	//跳转学生注册
+	// 跳转学生注册
 	@RequestMapping("/registS")
 	public String registS() {
 		return "registS";
 	}
 
-	//执行学生注册
+	// 执行学生注册
 	@RequestMapping("/doRegistS")
-	public String doRegist(Student student, Model model) {
+	public String doRegist(Student student) {
 		System.out.println(student);
 		try {
 			teacherService.RegistS(student);
@@ -72,18 +73,18 @@ public class TeacherController {
 		}
 	}
 
-	//跳转教师注册
+	// 跳转教师注册
 	@RequestMapping("/registT")
 	public String registT(Teacher teacher) {
-		if (teacher.getGid() != 2 || teacher.getGid() != 1) {
+		if (teacher.getGid() >= 3) {
 			return "error";
 		}
 		return "registT";
 	}
-	
-	//执行教师注册
+
+	// 执行教师注册
 	@RequestMapping("/doRegistT")
-	public String doRegist(Teacher teacher, Model model) {
+	public String doRegist(Teacher teacher) {
 		System.out.println(teacher);
 		try {
 			teacherService.RegistT(teacher);
@@ -91,17 +92,17 @@ public class TeacherController {
 		} catch (Exception e) {
 			return "error";
 		}
-	} 
-	
-	//修改信息
+	}
+
+	// 修改信息
 	@RequestMapping("/updateT")
 	public String teacherUpdate() {
 		return "updateT";
 	}
 
-	//执行修改
+	// 执行修改
 	@RequestMapping("/doUpdate")
-	public String doUpdate(Teacher teacher, Model model) {
+	public String doUpdate(Teacher teacher) {
 		System.out.println("当前用户：" + teacher.getT_name());
 		try {
 			teacherService.updateTeac(teacher);
@@ -120,7 +121,7 @@ public class TeacherController {
 
 	// 修改密码执行
 	@RequestMapping("/doUpdatePW")
-	public String doUpdatePW(Teacher teacher, Model model) {
+	public String doUpdatePW(Teacher teacher) {
 		System.out.println("当前用户：" + teacher.getT_name());
 		try {
 			teacherService.updateTPW(teacher);
@@ -130,5 +131,21 @@ public class TeacherController {
 			return "error";
 		}
 	}
-	
+
+	@RequestMapping("/showpage")
+	public String selByPage(Model model, HttpServletRequest request) {
+		String s_name = request.getParameter("s_name");
+		String t_name = request.getParameter("t_name");
+		String pageSize = request.getParameter("pageSize");
+		String pageNumber = request.getParameter("pageNumber");
+		
+		PageInfo pInfo = teacherService.showPage(s_name, t_name, pageSize, pageNumber);
+		
+		model.addAttribute("pInfo", pInfo);
+		
+		
+		return "showpage";
+
+	}
+
 }
